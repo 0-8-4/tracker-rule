16ea6a35cdc5f119d3f1b5e95aa86f0615e538d3
0-8-4 <ljd69154@liangjundi.cn>
2024-04-14 15:58:31 +1000

    update the main file

--- tracker.py
+++ tracker.py
@@ -2,10 +2,11 @@ import requests
 from urllib.parse import urlparse
 import ipaddress
 
-
 def get_tracker_list(tracker_urls):
-    url = requests.get(tracker_urls)
-    tracker_list = url.text.split('\n')
+    tracker_list = []
+    for u in tracker_urls:
+        url = requests.get(u)
+        tracker_list += url.text.split('\n')
     return tracker_list
 
 
@@ -16,6 +17,7 @@ def process_tracker(tracker_list):
     tracker_list = [x for i in tracker_list if (x := i.strip()) != '']
 
     tracker_parts_list = []
+    hostnames = set()  # 使用集合来存储已经出现过的 hostname
     for tracker in tracker_list:
         t = urlparse(tracker)
 
@@ -25,7 +27,10 @@ def process_tracker(tracker_list):
             'hostname': t.hostname,
         }
 
-        tracker_parts_list.append(tracker_parts)
+        # 在添加 tracker_parts 之前检查 hostname 是否已经存在
+        if t.hostname not in hostnames:
+            tracker_parts_list.append(tracker_parts)
+            hostnames.add(t.hostname)  # 添加新的 hostname 到集合中
 
     return tracker_parts_list
 
@@ -63,7 +68,7 @@ def output_tracker(ipv4, ipv6, server, dns):
         for t in ipv6:
             line = f'{t.get("hostname")}/128\n'
             f.write(line)
-    with open('server.txt', 'w') as f:
+    with open('host.conf', 'w') as f:
         for t in server:
             line = f'server=/{t.get("hostname")}/{dns}\n'
             f.write(line)
@@ -72,10 +77,15 @@ def output_tracker(ipv4, ipv6, server, dns):
 
 
 if __name__ == '__main__':
-    tracker_urls = 'https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt'
+    tracker_urls = [
+        'https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt',
+        'https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt',
+        'https://newtrackon.com/api/stable',
+        'http://github.itzmx.com/1265578519/OpenTracker/master/tracker.txt'
+    ]
     tracker_list = get_tracker_list(tracker_urls)
     process_tracker(tracker_list)
     tracker_parts = process_tracker(tracker_list)
     ipv4, ipv6, server = process_hostname(tracker_parts)
 
-    output_tracker(ipv4, ipv6, server, '8.8.8.8')
+    output_tracker(ipv4, ipv6, server, '114.114.114.114')
